8066e4b37710f75c81016e5ee0fb91d5144ee4ef
AlphaJack <alphajack@tuta.io>
2024-09-16 01:37:27 +0200

    test: simplified test using modern rehype

--- test.js
+++ test.js
@@ -1,25 +1,22 @@
-import { strict as assert } from 'assert';
-import { unified } from 'unified';
-import rehypeParse from 'rehype-parse';
-import rehypeStringify from 'rehype-stringify';
-import rehypeThumbhash from './index.js';
+import { strict as assert } from "assert";
+import { rehype } from "rehype";
+import rehypeThumbhash from "./index.js";
 
 (async () => {
   try {
-    const expectedOutput = '<img src="example.jpg" data-thumbhash="ZhgODYKHh3l/ioh0d5hohkVtEOYG">';
-
-    const file = await unified()
-      .use(rehypeParse, { fragment: true })
-      .use(rehypeThumbhash, { dir: './' })
-      .use(rehypeStringify)
+    const expectedOutput =
+      '<html><head></head><body><img src="example.jpg" data-thumbhash="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAAAAAAgCAYAAAA8J3kkAAAAxklEQVR4AQABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAABAP7/AAEBAP7/AAAgAAHflHy2AAAAAElFTkSuQmCC"></body></html>';
+    console.time("converted-in");
+    const file = await rehype()
+      .use(rehypeThumbhash, { dir: "./" })
       .process('<img src="example.jpg">');
 
     const output = String(file).trim();
-
+    console.timeEnd("converted-in");
     assert.strictEqual(output, expectedOutput);
-    console.log('Test passed: <img src="example.jpg" data-thumbhash="ZhgODYKHh3l/ioh0d5hohkVtEOYG">');
+    console.log("Test passed:", output);
   } catch (err) {
-    console.error('Test failed:', err);
+    console.error("Test failed:", err);
     process.exit(1); // Exit with status code 1 when the test fails
   }
 })();
