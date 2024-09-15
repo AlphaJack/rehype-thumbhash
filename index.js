0b47bfca74f864f575574307b888fcba76ad8bc3
AlphaJack <alphajack@tuta.io>
2024-09-16 01:27:54 +0200

    feat: added canvas image downscaler

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import path from 'path';
-import sharp from 'sharp';
-import { visit } from 'unist-util-visit';
-import { rgbaToThumbHash } from 'thumbhash';
+import { visit } from "unist-util-visit";
+import { rgbaToThumbHash, thumbHashToDataURL } from "thumbhash";
+//import { createCanvas, loadImage } from "@napi-rs/canvas";
+import sharp from "sharp";
+import path from "path";
 
 export default setImageThumbhash;
 
@@ -21,19 +22,23 @@ const absolutePathRegex = /^(?:[a-z]+:)?\/\//;
  *
  * @param {Object} options - Plugin options
  * @param {string} [options.dir] - Directory containing the images
+ * @param {string} [options.originalAttribute] - Attribute pointing to the original image
+ * @param {string} [options.thumbhashAttribute] - Attribute pointing to the thumbhash image
  * @returns {Function} A transformer function to be used with rehype
  */
 function setImageThumbhash(options) {
   const opts = options || {};
   const dir = opts.dir;
+  const originalAttribute = opts.originalAttribute || "src";
+  const thumbhashAttribute = opts.thumbhashAttribute || "data-thumbhash";
   const thumbhashCache = {};
 
   return async function transformer(tree, file) {
     const images = [];
 
     // Collect all image nodes
-    visit(tree, 'element', (node) => {
-      if (node.tagName === 'img' && node.properties && node.properties.src) {
+    visit(tree, "element", (node) => {
+      if (node.tagName === "img" && node.properties && node.properties.src) {
         images.push(node);
       }
     });
@@ -44,33 +49,36 @@ function setImageThumbhash(options) {
         const src = node.properties.src;
 
         // Check if thumbhash is already in cache
-        let base64thumbhash = thumbhashCache[src];
-        if (!base64thumbhash) {
-          base64thumbhash = (await getImageThumbhash(src, dir)) || '';
-          thumbhashCache[src] = base64thumbhash;
+        let thumbhashDataURL = thumbhashCache[src];
+        if (!thumbhashDataURL) {
+          thumbhashDataURL = (await getImageThumbhash(src, dir)) || "";
+          thumbhashCache[src] = thumbhashDataURL;
         }
 
         // Ensure properties object exists
         if (!node.properties) {
           node.properties = {};
         }
-        node.properties['data-thumbhash'] = base64thumbhash;
-      })
+        if (thumbhashDataURL !== "") {
+          node.properties[originalAttribute] = src;
+          node.properties[thumbhashAttribute] = thumbhashDataURL;
+        }
+      }),
     );
   };
 }
 
-  /**
-   * Asynchronously generates a thumbhash for a given image file.
-   * @param {string} src - The path to the image file.
-   * @param {string} [dir] - The directory containing the image file.
-   * @returns {Promise<string|null>} The thumbhash as a base64-encoded string,
-   *     or `null` if the image could not be processed.
-   */
+/**
+ * Asynchronously generates a thumbhash for a given image file.
+ * @param {string} src - The path to the image file.
+ * @param {string} [dir] - The directory containing the image file.
+ * @returns {Promise<string|null>} The thumbhash as a base64-encoded string,
+ *     or `null` if the image could not be processed.
+ */
 async function getImageThumbhash(src, dir) {
   try {
     if (!src) {
-      console.error('[rehype-thumbhash] Image src is undefined or null.');
+      console.error("[rehype-thumbhash] Image src is undefined or null.");
       return null;
     }
 
@@ -80,34 +88,75 @@ async function getImageThumbhash(src, dir) {
     }
 
     // Determine if path should be joined with the directory
-    const shouldJoin = !path.isAbsolute(src) || src.startsWith('/');
+    const shouldJoin = !path.isAbsolute(src) || src.startsWith("/");
     if (dir && shouldJoin) {
       src = path.join(dir, src);
     }
 
-    // Thumbhash doesn't process images larger than 100x1004
-    const maxDimension = 100;
-    let image = sharp(src).rotate();
-
-    // Resize the image to fit within 100x100 pixels if necessary
-    image = image.resize(maxDimension, maxDimension, { fit: 'inside' });
-
-    // Ensure the image is in RGBA format
-    image = image.ensureAlpha();
-
-    // Get the raw pixel data and new dimensions
-    const { data: rgbaPixels, info } = await image
-    .raw()
-    .toBuffer({ resolveWithObject: true });
-    const { width, height } = info;
+    // Thumbhash doesn't support images larger than 100x100, so we downscale it
+    const { resizedWidth, resizedHeight, rgba } =
+      await downscaleImageSharp(src);
+    //const { resizedWidth, resizedHeight, rgba } = await downscaleImageCanvas(src)
 
     // Generate thumbhash
-    const hash = rgbaToThumbHash(width, height, rgbaPixels);
-    const base64thumbhash = Buffer.from(hash).toString('base64');
-    return base64thumbhash;
+    const thumbhashBinary = rgbaToThumbHash(resizedWidth, resizedHeight, rgba);
+    //const thumbhashBase64 = Buffer.from(thumbhashBinary).toString('base64');
+    const thumbhashDataURL = thumbHashToDataURL(thumbhashBinary);
+
+    return thumbhashDataURL;
   } catch (error) {
-    console.error('[rehype-thumbhash] Skipped processing image due to error:', src);
+    console.error(
+      "[rehype-thumbhash] Skipped processing image due to error:",
+      src,
+    );
     console.debug(error);
     return null;
   }
 }
+
+/**
+ * Downscale an image to fit within a square of size 100x100. The image is
+ * rotated if necessary, resized with the "inside" fit strategy, and ensured
+ * to have an alpha channel.
+ * @param {string} src - The path to the image file.
+ * @returns {Promise<{width: number, height: number, rgbaPixels: Uint8Array}>}
+ *     The downscaled image dimensions and its raw RGBA pixel data.
+ */
+async function downscaleImageSharp(src) {
+  const maxSize = 100;
+  let image = sharp(src).rotate();
+  image = image.resize(maxSize, maxSize, { fit: "inside" });
+  image = image.ensureAlpha();
+  const { data: rgbaPixels, info } = await image
+    .raw()
+    .toBuffer({ resolveWithObject: true });
+  const { width, height } = info;
+  return { width, height, rgbaPixels };
+}
+
+/**
+ * Downscale an image to fit within a square of size 100x100.
+ * @param {string} src - The path to the image file.
+ * @returns {Promise<{width: number, height: number, rgbaPixels: Uint8Array}>}
+ *     The downscaled image dimensions and its raw RGBA pixel data.
+ */
+/*
+async function downscaleImageCanvas(src) {
+  const maxSize = 100;
+  const image = await loadImage(src);
+  const width = image.width;
+  const height = image.height;
+
+  const scale = Math.min(maxSize / width, maxSize / height);
+  const resizedWidth = Math.floor(width * scale);
+  const resizedHeight = Math.floor(height * scale);
+
+  const canvas = createCanvas(resizedWidth, resizedHeight);
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(image, 0, 0, resizedWidth, resizedHeight);
+
+  const imageData = ctx.getImageData(0, 0, resizedWidth, resizedHeight);
+  const rgba = new Uint8Array(imageData.data.buffer);
+  return { resizedWidth, resizedHeight, rgba };
+}
+*/
